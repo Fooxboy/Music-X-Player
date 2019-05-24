7e5d2f42238ae8d9c88e893a693467001d2715a1
Fooxboy <fooxboy@yandex.ru>
2019-05-24 20:09:00 +0300

    add frames

---
+++ Fooxboy.MusicX.Uwp/StaticContent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fooxboy.MusicX.Uwp.Services;
+
+namespace Fooxboy.MusicX.Uwp
+{
+    public class StaticContent
+    {
+        public NavigationService NavigationContentService { get; set; }
+        public NavigationService NavigationPlayerService { get; set; }
+    }
+}
