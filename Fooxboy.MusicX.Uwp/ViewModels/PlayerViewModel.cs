10476c52f8110a88db14abcf0c5e50c769e1c4cd
Fooxboy <fooxboy@yandex.ru>
2019-05-25 16:02:14 +0300

    Алалалалалмамамам

--- Fooxboy.MusicX.Uwp/ViewModels/PlayerViewModel.cs
+++ Fooxboy.MusicX.Uwp/ViewModels/PlayerViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Fooxboy.MusicX.Uwp.Enums;
+using Fooxboy.MusicX.Uwp.Interfaces;
 
 namespace Fooxboy.MusicX.Uwp.ViewModels
 {
@@ -25,7 +27,86 @@ namespace Fooxboy.MusicX.Uwp.ViewModels
         /// </summary>
         private PlayerViewModel()
         {
+            StaticContent.AudioService.PlayStateChanged += AudioServicePlayStateChanged;
+            StaticContent.AudioService.PositionChanged += AudioServicePositionChanged;
+            StaticContent.AudioService.CurrentAudioChanged += AudioServiceCurrentAudioChanged;
+        }
 
+
+        public bool IsPlaying
+        {
+            get { return StaticContent.AudioService.IsPlaying; }
+            set
+            {
+                //аоаоаомммм
+            }
+        }
+
+        public IAudio CurrentAudio => StaticContent.AudioService.CurrentPlaylist.CurrentItem;
+        public TimeSpan Position => StaticContent.AudioService.Position;
+        public double PositionSeconds
+        {
+            get { return StaticContent.AudioService.Position.TotalSeconds; }
+            set
+            {
+                if (StaticContent.AudioService.Position.TotalSeconds == value)
+                    return;
+
+                StaticContent.AudioService.Seek(TimeSpan.FromSeconds(value));
+            }
+        }
+
+        public TimeSpan Duration => StaticContent.AudioService.Duration;
+
+        public double DurationSeconds => StaticContent.AudioService.Duration.TotalSeconds;
+
+        public double Volume
+        {
+            get { return StaticContent.AudioService.Volume * 100f; }
+            set
+            {
+                var v = value / 100f;
+                if (StaticContent.AudioService.Volume == v)
+                    return;
+
+                StaticContent.AudioService.Volume = v;
+                StaticContent.Volume = v;
+                Changed("Volume");
+            }
+        }
+
+        public bool Repeat
+        {
+            get { return StaticContent.AudioService.Repeat == RepeatMode.Always; }
+            set
+            {
+                StaticContent.AudioService.Repeat = value ? RepeatMode.Always : RepeatMode.None;
+                StaticContent.Repeat = StaticContent.AudioService.Repeat;
+            }
+        }
+
+        private void AudioServicePlayStateChanged(object sender, EventArgs e)
+        {
+
+            Changed("IsPlaying");
+
+            //Обновление плиточки
+            //TileHelper.UpdateIsPlaying(IsPlaying);
+        }
+
+        private  void AudioServiceCurrentAudioChanged(object sender, EventArgs e)
+        {
+
+            Changed("CurrentAudio");
+            //TODO сделать обновление обложки
+        }
+
+        private void AudioServicePositionChanged(object sender, TimeSpan position)
+        {
+            Changed("Position");
+            Changed("PositionSeconds");
+            Changed("Duration");
+            Changed("DurationSeconds");
         }
 
 
