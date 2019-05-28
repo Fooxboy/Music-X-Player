9de6f5c11b8b658bd9e34f3a17cdc0b54f92bd8f
Fooxboy <fooxboy@yandex.ru>
2019-05-28 09:08:36 +0300

    added alah

---
+++ Fooxboy.MusicX.Uwp/Services/PlayMusicService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fooxboy.MusicX.Uwp.Models;
+
+namespace Fooxboy.MusicX.Uwp.Services
+{
+    public static class PlayMusicService
+    {
+        public async static void PlayMusicForLibrary(AudioFile audioFile)
+        {
+            var lastPlayPlaylist = await PlaylistsService.GetById(1);
+            if (!(lastPlayPlaylist.Tracks.Any(t => t.Source == audioFile.Source))) lastPlayPlaylist.Tracks.Add(audioFile);
+        }
+    }
+}
