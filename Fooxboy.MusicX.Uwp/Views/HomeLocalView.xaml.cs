696b706974f6514c69eaeab22be88d4a3da30f1d
Fooxboy <fooxboy@yandex.ru>
2019-06-03 09:11:24 +0300

    При скролинге изчезает панель с плейлистами

--- Fooxboy.MusicX.Uwp/Views/HomeLocalView.xaml.cs
+++ Fooxboy.MusicX.Uwp/Views/HomeLocalView.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Fooxboy.MusicX.Uwp.Services;
 using Fooxboy.MusicX.Uwp.ViewModels;
+using Microsoft.Toolkit.Uwp.UI.Animations;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -28,12 +29,40 @@ namespace Fooxboy.MusicX.Uwp.Views
         {
             this.InitializeComponent();
             HomeViewModel = HomeLocalViewModel.Instanse;
+
+            
         }
 
         public HomeLocalViewModel HomeViewModel { get; set; }
 
 
-        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        public static IEnumerable<DependencyObject> GetDescendants(DependencyObject start)
+        {
+            var queue = new Queue<DependencyObject>();
+            var count = VisualTreeHelper.GetChildrenCount(start);
+
+            for (int i = 0; i < count; i++)
+            {
+                var child = VisualTreeHelper.GetChild(start, i);
+                yield return child;
+                queue.Enqueue(child);
+            }
+
+            while (queue.Count > 0)
+            {
+                var parent = queue.Dequeue();
+                var count2 = VisualTreeHelper.GetChildrenCount(parent);
+
+                for (int i = 0; i < count2; i++)
+                {
+                    var child = VisualTreeHelper.GetChild(parent, i);
+                    yield return child;
+                    queue.Enqueue(child);
+                }
+            }
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs ee)
         {
             if (HomeViewModel.Playlists.Count == 0)
             {
@@ -46,6 +75,22 @@ namespace Fooxboy.MusicX.Uwp.Views
                 HomeViewModel.CountMusic();
             }
 
+            var scrollViewer = GetDescendants(MusicListView).OfType<ScrollViewer>().FirstOrDefault();
+
+
+            
+            scrollViewer.ViewChanging += (o, e)  =>
+            {
+                //PlaylistsGrid.Visibility = Visibility.Collapsed;
+                if (scrollViewer.VerticalOffset < 20)
+                {
+                    PlaylistsGrid.Visibility = Visibility.Visible;
+                }else
+                {
+                    PlaylistsGrid.Visibility = Visibility.Collapsed;
+                }
+
+            };
         }
     }
 }
