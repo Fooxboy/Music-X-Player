8827dda582d4be93f9e8998ca96ab7f03902d815
Fooxboy <fooxboy@yandex.ru>
2019-06-04 15:37:15 +0300

    сохранение плейлистов

--- Fooxboy.MusicX.Uwp/Services/PlaylistsService.cs
+++ Fooxboy.MusicX.Uwp/Services/PlaylistsService.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Fooxboy.MusicX.Core;
 using Fooxboy.MusicX.Uwp.Models;
 using Newtonsoft.Json;
 using Windows.Storage;
@@ -28,6 +29,27 @@ namespace Fooxboy.MusicX.Uwp.Services
             var json = await FileIO.ReadTextAsync(file);
             var model = JsonConvert.DeserializeObject<PlaylistFile>(json);
             return model;
+        }
+
+        public static async Task SavePlaylist(PlaylistFile playlist)
+        {
+            var pathPlaylists = await ApplicationData.Current.LocalFolder.GetFolderAsync("Playlists");
+            StorageFile filePlaylist;
+            if (pathPlaylists.TryGetItemAsync($"Id{playlist.Id}.json") == null)
+            {
+                filePlaylist = await pathPlaylists.CreateFileAsync($"Id{playlist.Id}.json");
+            }else
+            {
+                filePlaylist = await pathPlaylists.GetFileAsync($"Id{playlist.Id}.json");
+            }
+            var jsonString = JsonConvert.SerializeObject(playlist);
+            try
+            {
+                await FileIO.WriteTextAsync(filePlaylist, jsonString);
+            }catch(Exception e)
+            {
+                Log.Error(e);
+            }
         }
 
         public static async Task SetPlaylistLocal()
