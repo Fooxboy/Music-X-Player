a6de52f309f4de55fed81152fbe7b15763065ae2
Fooxboy <fooxboy@yandex.ru>
2019-06-05 09:48:18 +0300

    Пофиксил обложки

--- Fooxboy.MusicX.Uwp/Services/FindMetadataService.cs
+++ Fooxboy.MusicX.Uwp/Services/FindMetadataService.cs
@@ -48,11 +48,8 @@ namespace Fooxboy.MusicX.Uwp.Services
                 if (mp3File.Tag.Pictures.Any())
                 {
                     StorageFolder localFolder = ApplicationData.Current.LocalFolder;
-                    if( (await localFolder.GetFolderAsync("Covers")).TryGetItemAsync($"{audio.Id}.jpg") == null)
-                    {
-                        System.IO.File.WriteAllBytes($"{localFolder.Path}/Covers/{audio.Id}.jpg", mp3File.Tag.Pictures[0].Data.Data);
-
-                    }
+                    var alah = StaticContent.CoversFolder.TryGetItemAsync($"{audio.Id}.jpg");
+                    System.IO.File.WriteAllBytes($"{localFolder.Path}/Covers/{audio.Id}.jpg", mp3File.Tag.Pictures[0].Data.Data);
                     audio.Cover = $"{localFolder.Path}/Covers/{audio.Id}.jpg";
                 }
                 else
