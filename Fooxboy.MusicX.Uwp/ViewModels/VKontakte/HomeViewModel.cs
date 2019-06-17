5a50a957090487d0e2da5f71fc15ff9da9199c7b
Fooxboy <fooxboy@yandex.ru>
2019-06-17 14:56:49 +0300

    вью модел

---
+++ Fooxboy.MusicX.Uwp/ViewModels/VKontakte/HomeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fooxboy.MusicX.Uwp.ViewModels.VKontakte
+{
+    public class HomeViewModel
+    {
+        private static HomeViewModel instanse;
+
+        public static HomeViewModel Instanse
+        {
+            get
+            {
+                if (instanse == null) instanse = new HomeViewModel();
+
+                return instanse;
+            }
+        }
+    }
+}
