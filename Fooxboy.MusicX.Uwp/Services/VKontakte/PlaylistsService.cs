776f0d4c5e0a06aa8ab983cba2c7b89b3593171c
Fooxboy <fooxboy@yandex.ru>
2019-06-17 22:45:01 +0300

    Я че-то накодил но не знаю что

---
+++ Fooxboy.MusicX.Uwp/Services/VKontakte/PlaylistsService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fooxboy.MusicX.Core.Interfaces;
+using Fooxboy.MusicX.Uwp.Models;
+
+namespace Fooxboy.MusicX.Uwp.Services.VKontakte
+{
+    public static class PlaylistsService
+    {
+        public static PlaylistFile ConvertToPlaylistFile(IPlaylistFile playlist)
+        {
+            var playlistFile = new PlaylistFile()
+            {
+                Artist = playlist.Artist,
+                Cover = playlist.Cover,
+                IsLocal = false,
+                Tracks = playlist.Tracks,
+                Id = playlist.Id,
+                Name = playlist.Name,
+                TracksFiles = MusicService.ConvertToAudioFile(playlist.Tracks)
+            };
+
+            return playlistFile;
+        }
+    }
+}
--- Fooxboy.MusicX.Uwp/ViewModels/VKontakte/HomeViewModel.cs
+++ Fooxboy.MusicX.Uwp/ViewModels/VKontakte/HomeViewModel.cs
@@ -1,21 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-//using System.Linq;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Fooxboy.MusicX.Core.VKontakte.Music;
 using Fooxboy.MusicX.Uwp.Models;
 using Fooxboy.MusicX.Uwp.Services.VKontakte;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Input;
 
 namespace Fooxboy.MusicX.Uwp.ViewModels.VKontakte
 {
-    public class HomeViewModel
+    public class HomeViewModel:BaseViewModel
     {
         private static HomeViewModel instanse;
         private long maxCountElements = 0;
         const int countTracksLoading = 20;
+        private bool loadingPlaylists = true;
 
         public static HomeViewModel Instanse
         {
@@ -34,10 +36,22 @@ namespace Fooxboy.MusicX.Uwp.ViewModels.VKontakte
             Music = new LoadingCollection<AudioFile>();
             Music.OnMoreItemsRequested = GetMoreAudio;
             Music.HasMoreItemsRequested = HasMoreGetAudio;
+
+            Playlists = new LoadingCollection<PlaylistFile>();
+            Playlists.OnMoreItemsRequested = GetMorePlaylist;
+            Playlists.HasMoreItemsRequested = HasMoreGetPlaylists;
+
+
+            VisibilityNoTracks = Visibility.Collapsed;
+            VisibilityLoading = Visibility.Visible;
+            Changed("VisibilityNoTracks");
+            Changed("VisibilityLoading");
+            Changed("Music");
         }
 
 
         public LoadingCollection<AudioFile> Music { get; set; }
+        public LoadingCollection<PlaylistFile> Playlists { get; set; }
 
         public async Task<List<AudioFile>> GetMoreAudio(CancellationToken token, uint offset)
         {
@@ -46,16 +60,34 @@ namespace Fooxboy.MusicX.Uwp.ViewModels.VKontakte
             return music;
         }
 
-        public AudioFile SelectAudio { get; set; }
+        public async Task<List<PlaylistFile>> GetMorePlaylist(CancellationToken token, uint offset)
+        {
+            var playlistsVk = await Library.Playlists(10, 0);
+            var playlists = new List<PlaylistFile>();
+            foreach (var playlist in playlistsVk) playlists.Add(PlaylistsService.ConvertToPlaylistFile(playlist));
+            return playlists;
+        }
 
+        public PlaylistFile SelectPlaylist { get; set; }
+
+        public AudioFile SelectAudio { get; set; }
         public bool HasMoreGetAudio()
         {
             return true;
+        }
+
+        public bool HasMoreGetPlaylists()
+        {
+            return loadingPlaylists;
         }
 
         public async void MusicListView_Tapped(object sender, TappedRoutedEventArgs e)
         {
 
         }
+
+        public Visibility VisibilityNoTracks { get; set; }
+        public Visibility VisibilityLoading { get; set; }
+        public Visibility VisibilityPlaylists { get; set; }
     }
 }
