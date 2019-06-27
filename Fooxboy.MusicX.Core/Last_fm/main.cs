35df56c5e22a50b0618a0032606e68c13202c2da
Пушыстый Гхэй <dumbcatkatto@outlook.com>
2019-06-27 19:07:53 +0300

    f

---
+++ Fooxboy.MusicX.Core/Last_fm/main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IF.Lastfm.Core.Api;
+
+namespace Fooxboy.MusicX.Core.Last_fm
+{
+    public class main
+    {
+        public void init()
+        {
+            StaticContent.LastfmClient = new LastfmClient("", "");
+        }
+    }
+}
