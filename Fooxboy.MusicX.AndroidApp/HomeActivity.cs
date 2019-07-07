e8e32ab03cc9d1ed5dc16e15c9298871c2b4bf40
Fooxboy <fooxboy@yandex.ru>
2019-07-07 16:47:24 +0300

    Android: Добавлен home Activity

---
+++ Fooxboy.MusicX.AndroidApp/HomeActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Fooxboy.MusicX.AndroidApp
+{
+    [Activity(Label = "Домашний экран")]
+    public class HomeActivity:Activity
+    {
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+
+            SetContentView(Resource.Layout.homeActivity);
+        }
+    }
+}
