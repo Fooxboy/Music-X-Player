cf8cb2e09dc9657ccf4c2b5b5f0424fa863f79af
Fooxboy <fooxboy@yandex.ru>
2019-07-07 17:08:03 +0300

    Android: Fix px

--- Fooxboy.MusicX.AndroidApp/Resources/Resource.designer.cs
+++ Fooxboy.MusicX.AndroidApp/Resources/Resource.designer.cs
@@ -2455,8 +2455,8 @@ namespace Fooxboy.MusicX.AndroidApp
 			// aapt resource value: 0x7f090036
 			public const int SYM = 2131296310;
 			
-			// aapt resource value: 0x7f09009d
-			public const int action0 = 2131296413;
+			// aapt resource value: 0x7f0900a1
+			public const int action0 = 2131296417;
 			
 			// aapt resource value: 0x7f09007b
 			public const int action_bar = 2131296379;
@@ -2479,17 +2479,17 @@ namespace Fooxboy.MusicX.AndroidApp
 			// aapt resource value: 0x7f090059
 			public const int action_bar_title = 2131296345;
 			
-			// aapt resource value: 0x7f09009a
-			public const int action_container = 2131296410;
+			// aapt resource value: 0x7f09009e
+			public const int action_container = 2131296414;
 			
 			// aapt resource value: 0x7f09007c
 			public const int action_context_bar = 2131296380;
 			
-			// aapt resource value: 0x7f0900a1
-			public const int action_divider = 2131296417;
+			// aapt resource value: 0x7f0900a5
+			public const int action_divider = 2131296421;
 			
-			// aapt resource value: 0x7f09009b
-			public const int action_image = 2131296411;
+			// aapt resource value: 0x7f09009f
+			public const int action_image = 2131296415;
 			
 			// aapt resource value: 0x7f090003
 			public const int action_menu_divider = 2131296259;
@@ -2506,11 +2506,11 @@ namespace Fooxboy.MusicX.AndroidApp
 			// aapt resource value: 0x7f09005b
 			public const int action_mode_close_button = 2131296347;
 			
-			// aapt resource value: 0x7f09009c
-			public const int action_text = 2131296412;
+			// aapt resource value: 0x7f0900a0
+			public const int action_text = 2131296416;
 			
-			// aapt resource value: 0x7f0900aa
-			public const int actions = 2131296426;
+			// aapt resource value: 0x7f0900ae
+			public const int actions = 2131296430;
 			
 			// aapt resource value: 0x7f09005c
 			public const int activity_chooser_view_content = 2131296348;
@@ -2545,8 +2545,8 @@ namespace Fooxboy.MusicX.AndroidApp
 			// aapt resource value: 0x7f090062
 			public const int buttonPanel = 2131296354;
 			
-			// aapt resource value: 0x7f09009e
-			public const int cancel_action = 2131296414;
+			// aapt resource value: 0x7f0900a2
+			public const int cancel_action = 2131296418;
 			
 			// aapt resource value: 0x7f090044
 			public const int center = 2131296324;
@@ -2560,8 +2560,8 @@ namespace Fooxboy.MusicX.AndroidApp
 			// aapt resource value: 0x7f090072
 			public const int checkbox = 2131296370;
 			
-			// aapt resource value: 0x7f0900a6
-			public const int chronometer = 2131296422;
+			// aapt resource value: 0x7f0900aa
+			public const int chronometer = 2131296426;
 			
 			// aapt resource value: 0x7f09004d
 			public const int clip_horizontal = 2131296333;
@@ -2617,8 +2617,8 @@ namespace Fooxboy.MusicX.AndroidApp
 			// aapt resource value: 0x7f09002f
 			public const int end = 2131296303;
 			
-			// aapt resource value: 0x7f0900ac
-			public const int end_padder = 2131296428;
+			// aapt resource value: 0x7f0900b0
+			public const int end_padder = 2131296432;
 			
 			// aapt resource value: 0x7f09003e
 			public const int enterAlways = 2131296318;
@@ -2662,8 +2662,8 @@ namespace Fooxboy.MusicX.AndroidApp
 			// aapt resource value: 0x7f090061
 			public const int icon = 2131296353;
 			
-			// aapt resource value: 0x7f0900ab
-			public const int icon_group = 2131296427;
+			// aapt resource value: 0x7f0900af
+			public const int icon_group = 2131296431;
 			
 			// aapt resource value: 0x7f090039
 			public const int ifRoom = 2131296313;
@@ -2671,8 +2671,8 @@ namespace Fooxboy.MusicX.AndroidApp
 			// aapt resource value: 0x7f09005e
 			public const int image = 2131296350;
 			
-			// aapt resource value: 0x7f0900a7
-			public const int info = 2131296423;
+			// aapt resource value: 0x7f0900ab
+			public const int info = 2131296427;
 			
 			// aapt resource value: 0x7f090058
 			public const int italic = 2131296344;
@@ -2695,14 +2695,20 @@ namespace Fooxboy.MusicX.AndroidApp
 			// aapt resource value: 0x7f09001c
 			public const int listMode = 2131296284;
 			
+			// aapt resource value: 0x7f09009b
+			public const int listView1 = 2131296411;
+			
+			// aapt resource value: 0x7f09009d
+			public const int listView2 = 2131296413;
+			
 			// aapt resource value: 0x7f090060
 			public const int list_item = 2131296352;
 			
-			// aapt resource value: 0x7f0900ae
-			public const int masked = 2131296430;
+			// aapt resource value: 0x7f0900b2
+			public const int masked = 2131296434;
 			
-			// aapt resource value: 0x7f0900a0
-			public const int media_actions = 2131296416;
+			// aapt resource value: 0x7f0900a4
+			public const int media_actions = 2131296420;
 			
 			// aapt resource value: 0x7f09008b
 			public const int message = 2131296395;
@@ -2719,17 +2725,17 @@ namespace Fooxboy.MusicX.AndroidApp
 			// aapt resource value: 0x7f09008c
 			public const int navigation = 2131296396;
 			
-			// aapt resource value: 0x7f0900b0
-			public const int navigation_dashboard = 2131296432;
+			// aapt resource value: 0x7f0900b4
+			public const int navigation_dashboard = 2131296436;
 			
 			// aapt resource value: 0x7f090094
 			public const int navigation_header_container = 2131296404;
 			
-			// aapt resource value: 0x7f0900af
-			public const int navigation_home = 2131296431;
+			// aapt resource value: 0x7f0900b3
+			public const int navigation_home = 2131296435;
 			
-			// aapt resource value: 0x7f0900b1
-			public const int navigation_notifications = 2131296433;
+			// aapt resource value: 0x7f0900b5
+			public const int navigation_notifications = 2131296437;
 			
 			// aapt resource value: 0x7f09003a
 			public const int never = 2131296314;
@@ -2740,14 +2746,14 @@ namespace Fooxboy.MusicX.AndroidApp
 			// aapt resource value: 0x7f09001d
 			public const int normal = 2131296285;
 			
-			// aapt resource value: 0x7f0900a9
-			public const int notification_background = 2131296425;
+			// aapt resource value: 0x7f0900ad
+			public const int notification_background = 2131296429;
 			
-			// aapt resource value: 0x7f0900a3
-			public const int notification_main_column = 2131296419;
+			// aapt resource value: 0x7f0900a7
+			public const int notification_main_column = 2131296423;
 			
-			// aapt resource value: 0x7f0900a2
-			public const int notification_main_column_container = 2131296418;
+			// aapt resource value: 0x7f0900a6
+			public const int notification_main_column_container = 2131296422;
 			
 			// aapt resource value: 0x7f09004b
 			public const int parallax = 2131296331;
@@ -2773,11 +2779,11 @@ namespace Fooxboy.MusicX.AndroidApp
 			// aapt resource value: 0x7f090049
 			public const int right = 2131296329;
 			
-			// aapt resource value: 0x7f0900a8
-			public const int right_icon = 2131296424;
+			// aapt resource value: 0x7f0900ac
+			public const int right_icon = 2131296428;
 			
-			// aapt resource value: 0x7f0900a4
-			public const int right_side = 2131296420;
+			// aapt resource value: 0x7f0900a8
+			public const int right_side = 2131296424;
 			
 			// aapt resource value: 0x7f09000c
 			public const int save_image_matrix = 2131296268;
@@ -2881,8 +2887,8 @@ namespace Fooxboy.MusicX.AndroidApp
 			// aapt resource value: 0x7f09004a
 			public const int start = 2131296330;
 			
-			// aapt resource value: 0x7f09009f
-			public const int status_bar_latest_event_content = 2131296415;
+			// aapt resource value: 0x7f0900a3
+			public const int status_bar_latest_event_content = 2131296419;
 			
 			// aapt resource value: 0x7f090075
 			public const int submenuarrow = 2131296373;
@@ -2905,6 +2911,12 @@ namespace Fooxboy.MusicX.AndroidApp
 			// aapt resource value: 0x7f090068
 			public const int textSpacerNoTitle = 2131296360;
 			
+			// aapt resource value: 0x7f09009a
+			public const int textView1 = 2131296410;
+			
+			// aapt resource value: 0x7f09009c
+			public const int textView2 = 2131296412;
+			
 			// aapt resource value: 0x7f090099
 			public const int text_input_password_toggle = 2131296409;
 			
@@ -2914,8 +2926,8 @@ namespace Fooxboy.MusicX.AndroidApp
 			// aapt resource value: 0x7f090015
 			public const int textinput_error = 2131296277;
 			
-			// aapt resource value: 0x7f0900a5
-			public const int time = 2131296421;
+			// aapt resource value: 0x7f0900a9
+			public const int time = 2131296425;
 			
 			// aapt resource value: 0x7f09001b
 			public const int title = 2131296283;
@@ -2962,8 +2974,8 @@ namespace Fooxboy.MusicX.AndroidApp
 			// aapt resource value: 0x7f090016
 			public const int view_offset_helper = 2131296278;
 			
-			// aapt resource value: 0x7f0900ad
-			public const int visible = 2131296429;
+			// aapt resource value: 0x7f0900b1
+			public const int visible = 2131296433;
 			
 			// aapt resource value: 0x7f09003b
 			public const int withText = 2131296315;
@@ -3266,6 +3278,12 @@ namespace Fooxboy.MusicX.AndroidApp
 		
 		public partial class String
 		{
+			
+			// aapt resource value: 0x7f0a002e
+			public const int MusicTitleString = 2131361838;
+			
+			// aapt resource value: 0x7f0a002d
+			public const int PlaylistsTitleString = 2131361837;
 			
 			// aapt resource value: 0x7f0a0000
 			public const int abc_action_bar_home_description = 2131361792;
