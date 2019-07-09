1ded9dd6eb0d1d7ab7ebfdf79f480ceb929ae2bb
Пушыстый Гхэй <dumbcatkatto@outlook.com>
2019-07-09 15:49:55 +0300

    Android: сплешскрин

---
+++ Fooxboy.MusicX.AndroidApp/SplashActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace Fooxboy.MusicX.AndroidApp
+{
+    [Activity(Label = "SplashActivity", Theme = "@style/Splash", MainLauncher = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        // Simulates background work that happens behind the splash screen
+        async void SimulateStartup()
+        {
+            await Task.Delay(5000); // Simulate a bit of startup work.
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
