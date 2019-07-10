d9a4d74fa3c09f3cde1cd73aacb1101ded45c469
Пушыстый Гхэй <dumbcatkatto@outlook.com>
2019-07-10 10:26:15 +0300

    Android: заготовка к получению локальной музыки

---
+++ Fooxboy.MusicX.AndroidApp/Services/MusicService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Android.Provider;
+using Fooxboy.MusicX.AndroidApp.Models;
+
+namespace Fooxboy.MusicX.AndroidApp.Services
+{
+    public static class MusicService
+    {
+
+        public static List<AudioFile> GetLocal()
+        {
+            var tracks = new List<AudioFile>();
+            string dir = Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryMusic).ToString();
+            var files = Directory.GetFiles(dir);
+            foreach (var track in files)
+            {
+                
+            }
+
+            return tracks;
+
+        }
+        
+    }
+}
