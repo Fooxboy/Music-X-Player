abd115626ce9d7d3764b59668a0e6e2c38dac833
Fooxboy <fooxboy@yandex.ru>
2019-07-14 15:25:36 +0300

    Android: ImnageService теперь умеет скачивать фотки юзера и задавать это прям в imageview

--- Fooxboy.MusicX.AndroidApp/Services/ImagesService.cs
+++ Fooxboy.MusicX.AndroidApp/Services/ImagesService.cs
@@ -1,26 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
+using Android.Graphics;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Fooxboy.MusicX.Core.Interfaces;
+using ImageViews.Rounded;
 using Java.IO;
 using Java.Net;
+using Path = System.IO.Path;
 
 namespace Fooxboy.MusicX.AndroidApp.Services
 {
     public static class ImagesService
     {
-        // static string path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal)
-
         static string path = Xamarin.Essentials.FileSystem.AppDataDirectory;
         public static string CoverTrack(IAudioFile track)
         {
@@ -37,30 +37,61 @@ namespace Fooxboy.MusicX.AndroidApp.Services
                     {
                         client.DownloadFile(new Uri(track.Cover), filename);
                     }
-
-                    //var fileStream = System.IO.File.Create(filename);
-                    //int count;
-                    //var url = new URL(filename);
-                    //var connection = url.OpenConnection();
-                    //connection.Connect();
-                    //var lengthFile = connection.ContentLength;
-                    //var input = new BufferedInputStream(url.OpenStream(), lengthFile);
-                    //OutputStream output = new FileOutputStream(filename);
-                    //var data = new byte[1024];
-                    //long total = 0;
-                    //while ((count = input.Read(data)) != -1)
-                    //{
-                    //    total += count;
-                    //    output.Write(data, 0, count);
-                    //}
-
-                    //output.Flush();
-                    //output.Close();
-                    //input.Close();
                 }
 
                 return filename;
             }
+        }
+
+        public static string PhotoUser(string urlPhoto)
+        {
+            string filename = Path.Combine(path, $"User{urlPhoto.GetHashCode()}Photo.jpg");
+
+            if (!System.IO.File.Exists(filename))
+            {
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile(new Uri(urlPhoto), filename);
+                }
+            }
+            return filename;
+        }
+
+        public static void SetImageString(this RoundedImageView imageview, string pathImage, int decodePixelWidth, int decodePixelHeight)
+        {
+            var file = new File(pathImage);
+            var opt = new BitmapFactory.Options();
+            opt.InJustDecodeBounds = true;
+            BitmapFactory.DecodeFile(file.AbsolutePath, opt);
+            opt.InSampleSize = CalculateInSampleSize(opt, decodePixelWidth, decodePixelHeight);
+            opt.InJustDecodeBounds = false;
+            Bitmap myBitmap = BitmapFactory.DecodeFile(file.AbsolutePath, opt);
+            imageview.SetImageBitmap(myBitmap);
+        }
+        
+        public static int CalculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight)
+        {
+            // Начальная высота и ширина изображения
+            int height = options.OutHeight;
+            int width = options.OutWidth;
+            int inSampleSize = 1;
+
+            if (height > reqHeight || width > reqWidth)
+            {
+
+                int halfHeight = height / 2;
+                int halfWidth = width / 2;
+
+                // Рассчитываем наибольшее значение inSampleSize, которое равно степени двойки
+                // и сохраняем высоту и ширину, когда они больше необходимых
+                while ((halfHeight / inSampleSize) > reqHeight
+                       && (halfWidth / inSampleSize) > reqWidth)
+                {
+                    inSampleSize *= 2;
+                }
+            }
+
+            return inSampleSize;
         }
 
         public static string CoverPlaylist(IPlaylistFile playlist)
@@ -78,26 +109,6 @@ namespace Fooxboy.MusicX.AndroidApp.Services
                 {
                     client.DownloadFile(new Uri(uriImage), filename);
                 }
-
-                //var fileStream = System.IO.File.Create(filename);
-                //int count;
-                //var url = new URL(filename);
-                //var connection = url.OpenConnection();
-                //connection.Connect();
-                //var lengthFile = connection.ContentLength;
-                //var input = new BufferedInputStream(url.OpenStream(), lengthFile);
-                //OutputStream output = new FileOutputStream(filename);
-                //var data = new byte[1024];
-                //long total = 0;
-                //while((count = input.Read(data)) != -1)
-                //{
-                //    total += count;
-                //    output.Write(data, 0, count);
-                //}
-
-                //output.Flush();
-                //output.Close();
-                //input.Close();
             }
 
             return filename;
