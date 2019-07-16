7269882b86354a14710f521bc9eeaed0ddf049b9
Fooxboy <fooxboy@yandex.ru>
2019-07-17 02:11:38 +0300

    android: fix error

--- Fooxboy.MusicX.AndroidApp/Models/PlaylistFile.cs
+++ Fooxboy.MusicX.AndroidApp/Models/PlaylistFile.cs
@@ -4,23 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Fooxboy.MusicX.Core.Interfaces;
+using Fooxboy.MusicX.Core.Models;
 using Newtonsoft.Json;
 
 namespace Fooxboy.MusicX.AndroidApp.Models
 {
-    public class PlaylistFile : IPlaylistFile
+    public class PlaylistFile : PlaylistFileAnyPlatform
     {
-        public string Name { get; set; }
-        public string Artist { get; set; }
-        public long Id { get; set; }
-        public bool IsLocal { get; set; }
-        [JsonIgnore]
-        public IList<IAudioFile> Tracks { get; set; }
-        public List<AudioFile> TracksFiles { get; set; }
-        public string Cover { get; set; }
-        public bool IsAlbum { get; set; }
-        public string Genre { get; set; }
-        public string Year { get; set; }
-        public string Description { get; set; }
+       public List<AudioFile> TracksFiles { get; set; }
     }
 }
