6962b90c1c725ea77bba810630b9926e390f2223
Fooxboy <Fooxboy@yandex.ru>
2019-08-19 16:23:29 +0300

    uwp: block control

---
+++ Fooxboy.MusicX.Uwp/Models/Block.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fooxboy.MusicX.Core.Models;
+
+namespace Fooxboy.MusicX.Uwp.Models
+{
+    public class Block: BlockAnyPlatform
+    {
+    }
+}
