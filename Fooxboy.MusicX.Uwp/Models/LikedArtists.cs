17d0b93f66abd1bc11aeeb6db0a21f93a1a14d37
Fooxboy <Fooxboy@yandex.ru>
2019-08-28 19:56:51 +0300

    uwp: liked artists

---
+++ Fooxboy.MusicX.Uwp/Models/LikedArtists.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Toolkit.Uwp.UI.Controls.TextToolbarSymbols;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fooxboy.MusicX.Uwp.Models
+{
+    public class LikedArtist
+    {
+        public string Name { get; set; }
+        public string Banner { get; set; }
+        public long Id { get; set; }
+    }
+
+    public class LikedArtists
+    {
+        List<LikedArtist> Artists { get; set; }
+    }
+}
---
+++ Fooxboy.MusicX.Uwp/Services/VKontakte/LikedArtistsService.cs
@@ -0,0 +1,40 @@
+﻿using Fooxboy.MusicX.Uwp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fooxboy.MusicX.Uwp.Services.VKontakte
+{
+    public static class LikedArtistsService
+    {
+
+        public static async Task CreateLikedArtstsFile()
+        {
+            //TODO
+        }
+
+        public static async Task<bool> IsLikedArtist(long artistId)
+        {
+            //TODO
+            return true;
+        }
+
+        public static async Task<LikedArtists> GetLikedArtists()
+        {
+            //TODO
+            return null;
+        }
+
+        public static async Task RemoveArtist(long artistId)
+        {
+            //TODO
+        }
+
+        public static async Task AddLikedArtist(long artistId, string name, string banner)
+        {
+            //TODO
+        }
+    }
+}
