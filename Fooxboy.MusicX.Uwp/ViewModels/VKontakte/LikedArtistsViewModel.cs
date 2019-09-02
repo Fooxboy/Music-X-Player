73a1cf8fba4fcb7a4aec50610355e6045ea5c12d
Fooxboy <Fooxboy@yandex.ru>
2019-09-02 13:22:01 +0300

    uwp: liked Artists view

--- Fooxboy.MusicX.Uwp/ViewModels/VKontakte/LikedArtistsViewModel.cs
+++ Fooxboy.MusicX.Uwp/ViewModels/VKontakte/LikedArtistsViewModel.cs
@@ -1,17 +1,22 @@
 ﻿using Fooxboy.MusicX.Uwp.Models;
 using Fooxboy.MusicX.Uwp.Services.VKontakte;
+using Fooxboy.MusicX.Uwp.Views.VKontakte;
 using Microsoft.Toolkit.Uwp.UI.Controls.TextToolbarSymbols;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
 
 namespace Fooxboy.MusicX.Uwp.ViewModels.VKontakte
 {
     public class LikedArtistsViewModel:BaseViewModel
     {
         public List<LikedArtist> Artists { get; set; }
+
+        public LikedArtist SelectArtist { get; set; }
+        public Visibility NoArtistsVisibility { get; set; } = Visibility.Collapsed;
         public bool IsLoading { get; set; }
 
         public async Task StartLoading()
@@ -24,6 +29,17 @@ namespace Fooxboy.MusicX.Uwp.ViewModels.VKontakte
 
             IsLoading = false;
             Changed("IsLoading");
+            if (Artists.Count == 0)
+            {
+                NoArtistsVisibility = Visibility.Visible;
+                Changed("NoArtistsVisibility");
+            }
+        }
+
+        public void GoToArtist(object e, object o)
+        {
+            if (SelectArtist == null) return;
+            StaticContent.NavigationContentService.Go(typeof(ArtistView), new ArtistParameter() { Id=SelectArtist.Id, Name = SelectArtist.Name });
         }
     }
 }
