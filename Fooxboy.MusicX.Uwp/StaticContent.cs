4d5099cf7121cc9ba82cd32c666674655f7674d6
Fooxboy <Fooxboy@yandex.ru>
2019-12-17 15:09:24 +0300

    Переименовал навание картинок

--- Fooxboy.MusicX.Uwp/StaticContent.cs
+++ Fooxboy.MusicX.Uwp/StaticContent.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using Fooxboy.MusicX.Uwp.Enums;
-using Fooxboy.MusicX.Uwp.Models;
-using Fooxboy.MusicX.Uwp.Services;
-using Fooxboy.MusicX.Uwp.Utils.Extensions;
-using Microsoft.Toolkit.Uwp.UI.Controls.TextToolbarSymbols;
-using Windows.Storage;
-using Windows.UI.Xaml.Controls;
+﻿
 
 namespace Fooxboy.MusicX.Uwp
 {
