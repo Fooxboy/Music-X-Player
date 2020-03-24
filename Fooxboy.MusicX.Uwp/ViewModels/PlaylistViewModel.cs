54aa2ba59a342312e8fbe4a841767ab59186825f
Fooxboy <fooxboy@yandex.ru>
2020-03-24 20:48:32 +0300

    edit playlist view model

--- Fooxboy.MusicX.Uwp/ViewModels/PlaylistViewModel.cs
+++ Fooxboy.MusicX.Uwp/ViewModels/PlaylistViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fooxboy.MusicX.Uwp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace Fooxboy.MusicX.Uwp.ViewModels
 {
     public class PlaylistViewModel:BaseViewModel
     {
+
+        public Album Album { get; set; }
+
+        
     }
 }
