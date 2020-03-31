20b3e6ba9da683d008040084f821feae4eb9d954
1uckyCat <1uckycat@outlook.com>
2020-03-31 13:19:22 +0300

    The beginning of Music X Android: Reimagined

---
+++ Fooxboy.MusicX.Reimagined/Activities/MainActivity.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Fooxboy.MusicX.Reimagined.Services;
+using Fooxboy.MusicX.Core;
+using Android.Graphics.Drawables;
+
+namespace Fooxboy.MusicX.Reimagined
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            //--------set up base
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            //--------set up navig
+            BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
+            navigation.SetOnNavigationItemSelectedListener(this);
+
+            //--------login check
+            if (AuthService.IsLoggedIn())
+            {
+                Api.GetApi().VKontakte.Auth.Auto(AuthService.GetToken(), null);
+                var user = Api.GetApi().VKontakte.Users.Info.CurrentUser();
+            }
+            else
+            {
+                Dialog d = new Dialog(this);
+                d.Window.SetBackgroundDrawable(GetDrawable(Resource.Drawable.dialog));
+                d.SetCanceledOnTouchOutside(false);
+                d.SetContentView(Resource.Layout.dialog_auth);
+                d.Show();
+            }
+
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            var title = FindViewById<TextView>(Resource.Id.titlebar_title);
+            switch (item.ItemId)
+            {
+                case Resource.Id.navigation_home:
+                    title.Text = "Рекомендации";
+                    return true;
+                case Resource.Id.navigation_tracks:
+                    title.Text = "Ваша музыка";
+                    return true;
+                case Resource.Id.navigation_favourite:
+                    title.Text = "Избранное";
+                    return true;
+                case Resource.Id.navigation_search:
+                    title.Text = "Поиск";
+                    return true;
+                case Resource.Id.navigation_downloads:
+                    title.Text = "Загрузки";
+                    return true;
+            }
+            return false;
+        }
+    }
+}
+
---
+++ Fooxboy.MusicX.Reimagined/Services/AuthService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Fooxboy.MusicX.Reimagined.Services
+{
+    public static class AuthService
+    {
+        public static bool IsLoggedIn()
+        {
+            var prefs = Application.Context.GetSharedPreferences("MusicX", FileCreationMode.Private);
+            return String.IsNullOrEmpty(prefs.GetString("VKToken", null)) ? false : true;
+        }
+
+        public static string GetToken()
+        {
+            return Application.Context.GetSharedPreferences("MusicX", FileCreationMode.Private).GetString("VKToken", null);
+        }
+    }
+}
