6e8c37c8e2e324c2123de6189308acb7c51fd71a
Fooxboy <fooxboy@yandex.ru>
2020-04-05 17:06:59 +0300

    рппрмпмолррмо

---
+++ Fooxboy.MusicX.Uwp/ViewModels/UserInfoViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Threading.Tasks;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using VkNet.Abstractions;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+
+namespace Fooxboy.MusicX.Uwp.ViewModels
+{
+    public class UserInfoViewModel : ReactiveObject
+    {
+        private readonly IVkApi _vkApi;
+        private readonly ObservableAsPropertyHelper<string> _photo;
+        private readonly ObservableAsPropertyHelper<string> _fullName;
+
+        public UserInfoViewModel(IVkApi vkApi)
+        {
+            _vkApi = vkApi;
+            
+            var userObservable = LoadUserAsync().ToObservable().Where(user => user != null);
+            _photo = userObservable.Select(user => user.Photo100.ToString()).ToProperty(this, nameof(Photo), "https://docs.microsoft.com/windows/uwp/contacts-and-calendar/images/shoulder-tap-static-payload.png");
+            _fullName = userObservable.Select(user => $"{user.FirstName} {user.LastName}").ToProperty(this, nameof(FullName), "Имя фамилия");
+        }
+
+        public string Photo => _photo.Value;
+
+        public string FullName => _fullName?.Value;
+
+        private async Task<User> LoadUserAsync()
+        {
+            var user = await _vkApi.Users.GetAsync(new long[] { }, ProfileFields.Photo100).ConfigureAwait(false);
+            return user.FirstOrDefault();
+        }
+    }
+}
