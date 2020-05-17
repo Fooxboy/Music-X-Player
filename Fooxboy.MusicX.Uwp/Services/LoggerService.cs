9ff36c12c2620f809806d15e9f9c4929dafe8bd0
Fooxboy <fooxboy@yandex.ru>
2020-05-17 05:33:28 +0300

    Логгер

---
+++ Fooxboy.MusicX.Uwp/Services/LoggerService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fooxboy.MusicX.Uwp.Services
+{
+    public class LoggerService
+    {
+        public void Info(object msg)
+        {
+
+        }
+
+        public void Trace(object msg)
+        {
+
+        }
+
+        public void Error(object msg)
+        {
+
+        }
+    }
+}
